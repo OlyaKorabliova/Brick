d34abd6a3cc1ad45bf5f453701fd3e9b92ff76e0
RoksolyanaLebedenko <lebedenko@ucu.edu.ua>
2018-03-21 15:23:43 +0200

    It"s works!

--- src/Game.js
+++ src/Game.js
@@ -3,10 +3,41 @@ import * as most from "most"
 const height = 400;
 const width = 600;
 const animation = most.periodic(1000 / 60);
-// const keyPr = most.fromEvent('keydown', document).filter(el => el.key === 'ArrowLeft').observe(e=> console.log(e));
+
+const brickHeight = 22;
+const brickWidth = 50;
+let bricks = [];
+const bricksIntersections = (ball,vol) => {
+    for(let i =0; i < bricks.length;i++){
+        let brX1 = bricks[i][0];
+        let brX2 = brickWidth + brX1;
+        let brY1 = bricks[i][1];
+        let brY2 = brY1 + brickHeight;
+        if(brX1 <= ball[0] && ball[0] <= brX2){
+                if( (brY1 - 3 < ball[1] && ball[1] < brY1 + 3) ||
+                    (brY2 - 3 < ball[1] && ball[1] < brY2 + 3)){
+                    vol[1] = -vol[1];
+                    document.querySelectorAll(".Game__brick")[i].remove();
+                    bricks.splice(i,1);
+                    return vol;
+                }
+            }
+            if(brY1 <= ball[1] && ball[1] <= brY2){
+                if( (brX1 - 3 < ball[0] && ball[0] < brX1 + 3) ||
+                    (brX2 - 3 < ball[0] && ball[0] < brX2 + 3)){
+                    vol[0] = -vol[0];
+                    console.log(bricks[i],i);
+                    document.querySelectorAll(".Game__brick")[i].remove();
+                    bricks.splice(i,1);
+                    return vol;
+                }
+            }
+    }
+    return vol;
+};
 
 
-const Ball = (ball, x, y = 5) => {
+const Ball = (ball, x, y) => {
     ball.setAttribute("cx", x);
     ball.setAttribute("cy", y);
     let vy = 3;
@@ -18,12 +49,12 @@ const Ball = (ball, x, y = 5) => {
                 }
                 return nextX + vx;
             },
-            0)
+            x)
         .observe((a) => {
             x = a;
             ball.setAttribute("cx", a);
 
-        })
+        });
     const Ycoordinate = animation
         .scan((nextY) => {
                 if (nextY >= height) {
@@ -38,57 +69,26 @@ const Ball = (ball, x, y = 5) => {
                         vy=0;
                     }
                 }
-                const br = document.querySelector(".Game__brick");
-                const brX1 = parseInt(br.getAttribute("x"))+20;
-                const brX2 = parseInt(br.getAttribute("width")) + brX1;
-                console.log(brX1,brX2)
-                if (nextY < 0 || brX1 <= x && x <= brX2) {
+                    let vol =bricksIntersections([x,nextY],[vx,vy]);
+                    vx = vol[0];
+                    vy = vol[1];
+                if (nextY < 0) {
                     vy = -vy;
                 }
                 return nextY + vy;
             },
-            0).observe((a) => {
+            y).observe((a) => {
             ball.setAttribute("cy", a);
 
         });
-    // Ycoordinate.merge(Xcoordinate).subscribe()
 };
 
 const Platform = (platform, x, y) => {
     platform.setAttribute("x", x);
     platform.setAttribute("y", y);
     let X = x;
-    // const keyleft = most.fromEvent("keydown", document)
-    //     .filter(el => el.key === 'ArrowLeft');
-    // .scan((nextX) => nextX - 10, X);
-    // .observe(e => {
-    //         console.log(e);
-    //         X = e;
-    //         platform.setAttribute("x", e);
-    //     }
-    // );
     const keyDown = most.fromEvent('keydown', document);
-    const keyUp = most.fromEvent('keyup', document);
-    // const keyright = most.fromEvent("keydown", document)
-    //     .filter(el => el.key === 'ArrowRight');
-    // .scan((nextX) => nextX + 10, X);
-    //     // .observe(e => {
-    //         console.log(e);
-    //         X = e;
-    //         platform.setAttribute("x", e);
-    //     }
-    // );
     keyDown
-    // .tap(e=> console.log(e))
-    // .chain(action => {
-    //         if (action.key === 'ArrowRight') {
-    //             return keyright
-    //         }
-    //         else if (action.key === 'ArrowLeft') {
-    //             return keyleft;
-    //         }
-    //     }
-    // )
         .observe(action => {
             if (action.key === 'ArrowRight' && X < (width-platform.getAttribute("width"))) {
                 X += 20;
@@ -101,19 +101,37 @@ const Platform = (platform, x, y) => {
         });
 };
 
-const Brick = (brick, x, y) =>{
-    brick.setAttribute("x", x);
-    brick.setAttribute("y", y);
-}
+const Bricks = () =>{
+    let svg = document.querySelector(".Game");
+    let svgNS = svg.namespaceURI;
+    let coordinates = [];
+    const columns = 11;
+    const rows = 5;
+    for(let c =0; c < rows; c++) {
+        for (let i = 0; i < columns; i++) {
+            let rect = document.createElementNS(svgNS, 'rect');
+            rect.setAttribute('class', 'Game__brick');
+            rect.setAttribute('x', (brickWidth + 5) * i);
+            rect.setAttribute('y', (brickHeight + 5) * c);
+            rect.setAttribute('width', brickWidth);
+            rect.setAttribute('height', brickHeight);
+            rect.setAttribute('id', c*rows+i);
+            coordinates.push([(brickWidth + 5) * i,(brickHeight + 5) * c]);
+            svg.appendChild(rect);
+        }
+    }
+    bricks = coordinates;
+    return coordinates;
+};
 
 
 class Game {
     constructor() {
-        // Ball(document.querySelectorAll('.Game__ball')[0], width / 3 - 100);
+        const bricks = Bricks();
+        console.log(bricks);
         let pl = Platform(document.querySelector(".Game__platform"), 200, 400);
-        let brick = Brick(document.querySelector(".Game__brick"), 180, 200);
-        Ball(document.querySelectorAll('.Game__ball')[1], 2 * width / 3 - 100);
-        // Ball(document.querySelectorAll('.Game__ball')[2], width - 100);
+        Ball(document.querySelector('.Game__ball'), width / 2, height - 10);
+
     }
 }
 
